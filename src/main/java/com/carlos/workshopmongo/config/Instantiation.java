b9ffac1a2d47c926e4761de96411d4788a1ec7be
lacerdajcarlos <lacerda.jcarlos@gmail.com>
2025-01-14 18:01:30 -0300

    Endpoint para retornar os Posts dos usuarios e Obtendo post por ID

--- src/main/java/com/carlos/workshopmongo/config/Instantiation.java
+++ src/main/java/com/carlos/workshopmongo/config/Instantiation.java
@@ -35,14 +35,18 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-		Post post1 = new Post(null, sdf.parse("21/07/2024"), "Partiu Viajem", "Vou viajar para Sao Paulo!", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("22/07/2024"), "Bom dia", "Arcodei feliz hoje!", new AuthorDTO (maria));
-
+		Post post1 = new Post(null, sdf.parse("21/07/2024"), "Partiu Viajem", "Vou viajar para Sao Paulo!",
+				new AuthorDTO(maria));
 		
+		Post post2 = new Post(null, sdf.parse("22/07/2024"), "Bom dia", "Arcodei feliz hoje!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
+
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 
 	}
 
